878b899f5ca3d1132b554f291de2f2d05964beb8
huzhongyuan <759663671@qq.com>
2017-03-08 13:38:40 +0800

    我的模块进行布局

--- MyApplication/app/src/main/java/com/beidouapp/et/myapplication/ui/MinePage.java
+++ MyApplication/app/src/main/java/com/beidouapp/et/myapplication/ui/MinePage.java
@@ -19,6 +19,9 @@ public class MinePage extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.mine,null);
+        View view = inflater.inflate(R.layout.mine, null);
+
+
+        return view;
     }
 }
